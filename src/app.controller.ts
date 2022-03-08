08d589e6b716cf8e07a2ee9510bc708d80593979
Victor Mateus <victordiniz_@outlook.com>
2022-03-07 20:58:42 -0400

    Conexão RDS

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Device } from './device/entities/device.entity';
 
 @Controller()
 export class AppController {
--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,12 +1,31 @@
 import { createConnection } from 'typeorm';
 
+// ======== Conexão Localhost =================
+// export const databaseProviders = [
+//   {
+//     provide: 'DATABASE_CONNECTION',
+//     useFactory: async () =>
+//       await createConnection({
+//         type: 'mysql',
+//         host: 'localhost',
+//         port: 3306,
+//         username: 'root',
+//         password: 'victor123',
+//         database: 'db_eldorado',
+//         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+//         synchronize: true,
+//       }),
+//   },
+// ];
+
+// ======== Conexão AWS RDS =================
 export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: async () =>
       await createConnection({
         type: 'mysql',
-        host: 'localhost',
+        host: 'database-1.c4uoqzjv4wv0.sa-east-1.rds.amazonaws.com',
         port: 3306,
         username: 'root',
         password: 'victor123',
@@ -16,3 +35,8 @@ export const databaseProviders = [
       }),
   },
 ];
+
+// === Comando para conexão ssh na ec2 ===
+
+// Executar no mesmo diretório de "myaws.pem"
+// ssh -i "myaws.pem" ubuntu@ec2-18-231-183-53.sa-east-1.compute.amazonaws.com
